74e2bbada16e8991f79a1d83d9295997110c3963
Lee Lazarecky <leelazarecky@gmail.com>
2022-04-12 20:24:33 -0400

    Updates passion project section

--- pages/sections/OpenSourceProjectsSection.tsx
+++ pages/sections/OpenSourceProjectsSection.tsx
@@ -17,19 +17,17 @@ type Project = {
 
 const projects: Project[] = [
   {
-    name: "AuthQuest",
+    name: "Phoenix",
     description:
-      "An authorization and authentication server for users to use as a replacement for paid services such as Auth0 or Okta that follows" +
-      "the OAuth2 and OpenID specifications. " +
-      "This project comes with a customizable login and signup page that servers as an alternative to a custom login and signup page." +
-      "Also features an Administration dashboard that allows project owners to add in new authorization scopes to a user's OAuth2 token.",
+      "Phoenix is a Multi-System Emulation frontend that allows users to play all their favorite retro games on" +
+      " emulated consoles on their computer.",
     image: {
-      src: "/authquest.png",
+      src: "/phx.png",
       height: 500,
       width: 300,
     },
-    githubLink: "",
-    tags: ["Vue.js", "Kotlin", "MySql", "Java", "Spring Boot", "Design"],
+    githubLink: "https://github.com/team-phoenix/Phoenix",
+    tags: ["C++", "SQLite", "Emulation", "Qt", "QML", "CMake"],
   },
   {
     name: "RetroQuest",
@@ -59,18 +57,21 @@ const projects: Project[] = [
     githubLink: "",
     tags: ["Java", "Spring Boot", "React.js", "Design"],
   },
+
   {
-    name: "Phoenix",
+    name: "AuthQuest",
     description:
-      "Phoenix is a Multi-System Emulation frontend that allows users to play all their favorite retro games on" +
-      " emulated consoles on their computer.",
+      "An authorization and authentication server for users to use as a replacement for paid services such as Auth0 or Okta that follows" +
+      "the OAuth2 and OpenID specifications. " +
+      "This project comes with a customizable login and signup page that servers as an alternative to a custom login and signup page." +
+      "Also features an Administration dashboard that allows project owners to add in new authorization scopes to a user's OAuth2 token.",
     image: {
-      src: "/phx.png",
+      src: "/authquest.png",
       height: 500,
       width: 300,
     },
-    githubLink: "https://github.com/team-phoenix/Phoenix",
-    tags: ["C++", "SQLite", "Emulation", "Qt", "QML", "CMake"],
+    githubLink: "",
+    tags: ["Vue.js", "Kotlin", "MySql", "Java", "Spring Boot", "Design"],
   },
 ];
 
@@ -124,6 +125,42 @@ export default function OpenSourceProjectsSection() {
     );
   }
 
+  function ProjectDescription(project: Project) {
+    return (
+      <div className={"m-auto w-1/2 py-8"}>
+        <h1 className={"text-2xl md:text-3xl"}>{project.name}</h1>
+        {project.githubLink && project.githubLink !== "" && (
+          <Link href={"https://github.com/druage"}>
+            <a
+              target={"_blank"}
+              className="mt-2 mb-4 inline-flex items-center rounded-full bg-zinc-800 px-3 py-0.5 text-sm font-medium text-white hover:underline"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                height={16}
+                width={16}
+                className={"mr-1"}
+              />
+              View Github Project
+            </a>
+          </Link>
+        )}
+        <div className={"text-1xl md:text-1xl"}>{project.description}</div>
+
+        <div className={"mt-6 flex flex-wrap gap-3"}>
+          {project.tags.map((tag) => (
+            <span
+              key={tag}
+              className="inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div id={"open-source-projects"} className={"m-auto max-w-7xl pb-64"}>
       <Heading />
@@ -131,52 +168,17 @@ export default function OpenSourceProjectsSection() {
       <div className={"mt-2 grid grid-cols-1 gap-24 md:mt-8"}>
         {projects.map((it) => (
           <div key={it.name}>
-            <div
-              className={
-                "box-sh grid grid-cols-1 bg-white text-black md:grid-cols-2"
-              }
-            >
-              <div className={"relative h-[800px] w-full"}>
+            {ProjectDescription(it)}
+
+            <div className={"bg-white text-black"}>
+              <div className={"relative m-auto h-[700px] w-[1000px]"}>
                 <Image
                   src={it.image.src}
                   alt="project-image"
-                  objectFit="cover"
-                  className={"object-left-top"}
+                  objectFit="contain"
+                  className={"object-top"}
                   layout={"fill"}
                 />
-              </div>
-
-              <div className={"border-l border-gray-300 p-12"}>
-                <h1 className={"py-4 text-2xl md:text-3xl"}>{it.name}</h1>
-                <div className={"text-1xl md:text-1xl"}>{it.description}</div>
-
-                <div className={"mt-6 flex flex-wrap gap-3"}>
-                  {it.tags.map((tag) => (
-                    <span
-                      key={tag}
-                      className="inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800"
-                    >
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-                {it.githubLink && it.githubLink !== "" && (
-                  <Link href={it.githubLink}>
-                    <a
-                      type="button"
-                      target={"_blank"}
-                      className="mt-6 inline-flex items-center border border-transparent bg-zinc-700 px-6 py-2 text-base font-medium text-white shadow-sm hover:bg-zinc-700 hover:underline focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 md:text-lg"
-                    >
-                      <FontAwesomeIcon
-                        icon={faGithub}
-                        height={24}
-                        width={24}
-                        className={"mr-2"}
-                      />
-                      View Project on Github
-                    </a>
-                  </Link>
-                )}
               </div>
             </div>
           </div>
