376811e82b6793adc4a2114a2348eafc1ce1c936
Lee Lazarecky <leelazarecky@gmail.com>
2022-04-30 11:10:26 -0400

    Refactors Document back into index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,42 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 import AboutSection from "./components/sections/AboutSection";
 import OpenSourceProjectsSection from "./components/sections/OpenSourceProjectsSection";
 import FooterSection from "./components/sections/FooterSection";
 import SkillsSection from "./components/sections/SkillsSection";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <div id={"about"}>
+    return (
+        <div id={"about"}>
 
-      <AboutSection />
+            <Head>
+                <title>Lee Lazarecky</title>
+                <meta name="description" content="A portfolio for Lee Lazarecky"/>
+                <link rel="icon" href="/public/favicon.png"/>
 
-      <main className={"w-full border-t-2 border-gray-100"}>
-        <SkillsSection />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin={"true"}
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
 
-        <OpenSourceProjectsSection />
-      </main>
+            <AboutSection/>
 
-      <FooterSection />
-    </div>
-  );
+            <main className={"w-full border-t-2 border-gray-100"}>
+                <SkillsSection/>
+
+                <OpenSourceProjectsSection/>
+            </main>
+
+            <FooterSection/>
+        </div>
+    );
 };
 
 export default Home;
