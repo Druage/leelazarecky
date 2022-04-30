72b404c986ff39dff8d774b7d60f6d144d8310d3
Lee Lazarecky <leelazarecky@gmail.com>
2022-04-30 11:50:42 -0400

    Add resume download link

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
 
 export default function NavBar() {
-  return (
-    <header
-      className={
-        "flex flex-col items-center justify-center gap-x-12 gap-y-3 py-12 font-medium md:flex-row"
-      }
-    >
-      <Link href={"/"}>
-        <a className={"hover:text-blue-500 hover:underline"}>About</a>
-      </Link>
-      <Link href={"#technical-skills"}>
-        <a className={"hover:text-blue-500 hover:underline"}>Technical Skills</a>
-      </Link>
-      <Link href={"#open-source-projects"}>
-        <a className={"hover:text-blue-500 hover:underline"}>Passion Projects</a>
-      </Link>
-    </header>
-  );
+    return (
+        <header
+            className={
+                "flex flex-col items-center justify-center gap-x-12 gap-y-3 py-12 font-medium md:flex-row"
+            }
+        >
+            <Link href={"/"}>
+                <a className={"hover:text-blue-500 hover:underline"}>About</a>
+            </Link>
+            <Link href={"#technical-skills"}>
+                <a className={"hover:text-blue-500 hover:underline"}>Technical Skills</a>
+            </Link>
+            <Link href={"#open-source-projects"}>
+                <a className={"hover:text-blue-500 hover:underline"}>Passion Projects</a>
+            </Link>
+            <a download href={"/LeeLazarecky_Resume.pdf"} className={"hover:text-blue-500 hover:underline"}>Resume</a>
+        </header>
+    );
 }
